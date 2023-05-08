55b6e01d47a6d143a163bd426471a312f57c62a0
KrstinaPerovic <perovickrstina1@gmail.com>
2023-05-08 16:58:19 +0200

    kreiran i povezan model

---
+++ app/Models/Trip.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Trip extends Model
+{
+    use HasFactory;
+    protected $fillable = ['title', 'description', 'start_date', 'end_date','destination_id','user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function destination()
+    {
+        return $this->belongsTo(Destination::class);
+    }
+
+    public function activities()
+    {
+        return $this->hasMany(Activity::class);
+    }
+}
